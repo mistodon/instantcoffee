f4882d6de168411d5e187ebc6764d20513bd6793
Vi <1149443+mistodon@users.noreply.github.com>
2019-04-25 21:09:59 +0100

    Move parser to its own module

--- src/lib.rs
+++ src/lib.rs
@@ -1,162 +1,72 @@
-mod prelude;
+mod parse;
 
 use failure::{self, Error};
 
-#[allow(unused_imports)]
-use prelude::*;
+use parse::Parser;
 
+type Ident<'a> = &'a [u8];
+
+#[derive(Debug, PartialEq)]
+pub struct Path<'a>(Vec<Ident<'a>>);
+
+#[derive(Debug, PartialEq)]
+pub struct SymbolSoup<'a> {
+    pub idents: Vec<Ident<'a>>,
+}
+
+#[derive(Debug, PartialEq)]
 pub struct Parse<'a> {
-    pub package: Vec<&'a [u8]>,
+    pub source: &'a str,
+    pub package: Path<'a>,
+    pub imports: Vec<Import<'a>>,
+    pub import_span: [usize; 2],
+    pub classes: Vec<Class<'a>>,
 }
 
-pub struct Parser<'a> {
-    pub source: &'a [u8],
-    pub cursor: usize,
+#[derive(Debug, PartialEq)]
+pub struct Import<'a> {
+    pub path: Path<'a>,
+    pub is_static: bool,
+    pub star: bool,
 }
 
-impl<'a> Parser<'a> {
-    pub fn new(source: &'a str) -> Self {
-        Parser {
-            source: source.as_bytes(),
-            cursor: 0,
-        }
-    }
+#[derive(Debug, PartialEq)]
+pub struct Class<'a> {
+    pub annotations: Vec<Type<'a>>,
+    pub name: Ident<'a>,
+    pub fields: Vec<Field<'a>>,
+    pub methods: Vec<Method<'a>>,
+    pub type_params: Vec<Ident<'a>>,
+    pub subtypes: Vec<Type<'a>>,
+}
 
-    fn is_whitespace(&self, ch: u8) -> bool {
-        match ch {
-            b' ' | b'\t' | b'\r' => true,
-            _ => false,
-        }
-    }
+#[derive(Debug, PartialEq)]
+pub struct Type<'a> {
+    pub type_name: Path<'a>,
+    pub type_params: Vec<Type<'a>>,
+}
 
-    #[inline(always)]
-    pub fn finished(&self) -> bool {
-        self.cursor == self.source.len()
-    }
+#[derive(Debug, PartialEq)]
+pub struct Field<'a> {
+    pub annotations: Vec<Type<'a>>,
+    pub name: Ident<'a>,
+    pub field_type: Type<'a>,
+    pub value: SymbolSoup<'a>,
+}
 
-    pub fn skip_whitespace(&mut self) {
-        let mut in_comment = false;
-        loop {
-            // TODO: Multi-line comments
-            if self.skip_only(b"//") {
-                in_comment = true;
-            } else if self.skip_only(b"\n") {
-                in_comment = false;
-            } else {
-                if self.finished() || !(in_comment || self.is_whitespace(self.source[self.cursor])) {
-                    break;
-                }
+#[derive(Debug, PartialEq)]
+pub struct Method<'a> {
+    pub annotations: Vec<Type<'a>>,
+    pub name: Ident<'a>,
+    pub return_type: Type<'a>,
+    pub type_parameters: Vec<Ident<'a>>,
+}
 
-                self.cursor += 1;
-            }
-        }
-    }
-
-
-    #[inline(always)]
-    pub fn check(&self, next: &[u8]) -> bool {
-        let end = self.cursor + next.len();
-        end <= self.source.len() && &self.source[self.cursor..end] == next
-    }
-
-    pub fn check_matching<F>(&self, f: F) -> Option<&'a [u8]>
-    where
-        F: Fn(u8) -> bool
-    {
-        let mut cursor = self.cursor;
-        while f(self.source[cursor]) && cursor < self.source.len() {
-            cursor += 1;
-        }
-        let result = &self.source[self.cursor..cursor];
-        if result.is_empty() { None } else { Some(result) }
-    }
-
-    pub fn check_keyword(&mut self, keyword: &[u8]) -> bool {
-        fn is_ident_char(ch: u8) -> bool {
-            ch == b'_'
-                || (b'a' <= ch && ch <= b'z')
-                || (b'A' <= ch && ch <= b'Z')
-                || (b'0' <= ch && ch <= b'9')
-        }
-
-        let end = self.cursor + keyword.len();
-        self.check(keyword) && (end == self.source.len() || !is_ident_char(self.source[end]))
-    }
-
-    pub fn skip(&mut self, next: &[u8]) -> bool {
-        let skipped = self.check(next);
-        if skipped {
-            self.cursor += next.len();
-            self.skip_whitespace();
-        }
-        skipped
-    }
-
-    pub fn skip_matching<F>(&mut self, f: F) -> Option<&'a [u8]>
-    where
-        F: Fn(u8) -> bool
-    {
-        let result = self.check_matching(f);
-        if let Some(result) = result {
-            self.cursor += result.len();
-            self.skip_whitespace();
-        }
-        result
-    }
-
-    pub fn skip_only(&mut self, next: &[u8]) -> bool {
-        let skipped = self.check(next);
-        if skipped {
-            self.cursor += next.len();
-        }
-        skipped
-    }
-
-    pub fn skip_keyword(&mut self, keyword: &[u8]) -> bool {
-        let success = self.check_keyword(keyword);
-        if success {
-            self.cursor += keyword.len();
-            self.skip_whitespace();
-        }
-        success
-    }
-
-    #[inline(always)]
-    pub fn expect(&mut self, next: &[u8]) -> Result<(), Error> {
-        self.expect_with_fn(|parser| parser.skip(next))
-    }
-
-    #[inline(always)]
-    pub fn expect_only(&mut self, next: &[u8]) -> Result<(), Error> {
-        self.expect_with_fn(|parser| parser.skip_only(next))
-    }
-
-    #[inline(always)]
-    pub fn expect_keyword(&mut self, keyword: &[u8]) -> Result<(), Error> {
-        self.expect_with_fn(|parser| parser.skip_keyword(keyword))
-    }
-
-    fn expect_with_fn<F>(&mut self, f: F) -> Result<(), Error>
-    where
-        F: Fn(&mut Self) -> bool,
-    {
-        if self.finished() {
-            return Err(failure::err_msg(format!(
-                "Expected `{}` but reached the end of the file.",
-                "(TODO: Show correct expectation)"
-            )));
-        }
-
-        if !f(self) {
-            Err(failure::err_msg(format!(
-                "Expected `{}` but saw `{}`",
-                "(TODO: Show correct expectation)",
-                "(TODO: Show what was found instead)",
-            )))
-        } else {
-            Ok(())
-        }
-    }
+#[derive(Debug, PartialEq)]
+pub struct Arg<'a> {
+    pub annotations: Vec<Type<'a>>,
+    pub name: Ident<'a>,
+    pub arg_type: Type<'a>,
 }
 
 fn is_ident_char(ch: u8) -> bool {
@@ -167,22 +77,109 @@ fn is_ident_char(ch: u8) -> bool {
 }
 
 pub fn parse_file(source: &str) -> Result<Parse, Error> {
-    let mut parser = Parser::new(source);
+    let parser = &mut Parser::new(source);
     parser.skip_whitespace();
     parser.expect_keyword(b"package")?;
 
-    let mut package = vec![];
-    while let Some(ident) = parser.skip_matching(is_ident_char) {
-        package.push(ident);
-        if !parser.skip(b".") {
-            break
-        }
-    }
+    let package = parse_path(parser);
     parser.expect(b";")?;
 
+    let mut imports = vec![];
+    let import_start_point = parser.cursor();
+
+    while parser.skip_keyword(b"import") {
+        let is_static = parser.skip_keyword(b"static");
+        let path = parse_path(parser);
+        let star = parser.skip_only(b"*");
+
+        imports.push(Import {
+            path,
+            is_static,
+            star,
+        });
+
+        parser.expect(b";")?;
+    }
+
+    let import_end_point = parser.cursor();
+
     Ok(Parse {
-        package: package,
+        source,
+        package,
+        imports,
+        import_span: [import_start_point, import_end_point],
+        classes: vec![],
     })
+}
+
+fn parse_path<'a>(parser: &mut Parser<'a>) -> Path<'a> {
+    let mut path = vec![];
+    while let Some(ident) = parser.skip_matching(is_ident_char) {
+        path.push(ident);
+        if !parser.skip(b".") {
+            break;
+        }
+    }
+    Path(path)
+}
+
+pub fn format_imports(parse: &Parse) -> Result<String, Error> {
+    use std::fmt::Write;
+
+    fn format_import(import: &Import) -> String {
+        let static_label = if import.is_static { "static " } else { "" };
+        let star_label = if import.star { ".*" } else { "" };
+        let path = import.path.0.join(&b'.');
+        format!(
+            "import {}{}{};",
+            static_label,
+            unsafe { std::str::from_utf8_unchecked(&path) },
+            star_label
+        )
+    }
+
+    let statics = parse.imports.iter().filter(|import| !import.is_static);
+    let main_imports = statics
+        .clone()
+        .filter(|import| import.path.0[0] != b"java" && import.path.0[0] != b"javax")
+        .map(format_import)
+        .collect::<Vec<_>>();
+    let javax_imports = statics
+        .clone()
+        .filter(|import| import.path.0[0] == b"javax")
+        .map(format_import)
+        .collect::<Vec<_>>();
+    let java_imports = statics
+        .filter(|import| import.path.0[0] == b"java")
+        .map(format_import)
+        .collect::<Vec<_>>();
+    let statics = parse
+        .imports
+        .iter()
+        .filter(|import| import.is_static)
+        .map(format_import)
+        .collect::<Vec<_>>();
+
+    let mut buffer = String::new();
+    let b = &mut buffer;
+    for import in main_imports {
+        writeln!(b, "{}", &import)?;
+    }
+    writeln!(b)?;
+    for import in javax_imports {
+        writeln!(b, "{}", &import)?;
+    }
+    writeln!(b)?;
+    for import in java_imports {
+        writeln!(b, "{}", &import)?;
+    }
+    writeln!(b)?;
+    for import in statics {
+        writeln!(b, "{}", &import)?;
+    }
+    writeln!(b)?;
+
+    Ok(buffer)
 }
 
 #[cfg(test)]
@@ -194,11 +191,59 @@ mod tests {
         let tests: &[(&str, &[&[u8]])] = &[
             ("package a;", &[b"a"]),
             ("package a.b.c;", &[b"a", b"b", b"c"]),
-            ("package com.falseidolfactory.thing;", &[b"com", b"falseidolfactory", b"thing"]),
+            (
+                "package com.falseidolfactory.thing;",
+                &[b"com", b"falseidolfactory", b"thing"],
+            ),
         ];
 
         for (src, package) in tests {
-            assert_eq!(&parse_file(src).unwrap().package, package);
+            assert_eq!(parse_file(src).unwrap().package, Path(package.to_vec()));
         }
+    }
+
+    #[test]
+    fn parse_imports_success() {
+        let source = "
+            package com.com.com;
+
+            import a.b.c;
+            import a;
+
+            import java.util.List;
+            import java.concurrent.*;
+
+            import static java.util.Arrays.asList;
+        ";
+        let result = parse_file(source).unwrap();
+        let expected = &[
+            Import {
+                path: Path(vec![b"a", b"b", b"c"]),
+                is_static: false,
+                star: false,
+            },
+            Import {
+                path: Path(vec![b"a"]),
+                is_static: false,
+                star: false,
+            },
+            Import {
+                path: Path(vec![b"java", b"util", b"List"]),
+                is_static: false,
+                star: false,
+            },
+            Import {
+                path: Path(vec![b"java", b"concurrent"]),
+                is_static: false,
+                star: true,
+            },
+            Import {
+                path: Path(vec![b"java", b"util", b"Arrays", b"asList"]),
+                is_static: true,
+                star: false,
+            },
+        ];
+
+        assert_eq!(result.imports, expected);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    use std::io::Read;
+
+    let mut input = String::new();
+    std::io::stdin().read_to_string(&mut input).unwrap();
+
+    let parse = instantcoffee::parse_file(&input).unwrap();
+    let formatted_imports = instantcoffee::format_imports(&parse).unwrap();
+    let [start, end] = parse.import_span;
+    println!("{}{}{}", &input[..start], formatted_imports, &input[end..]);
 }
