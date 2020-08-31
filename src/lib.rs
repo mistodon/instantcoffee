77ee02c2a61fc75acf7378a90229e73c07ce4525
Claire Harris <wishing.engine@gmail.com>
2020-08-31 12:48:02 +0100

    WIP checkin

--- src/lib.rs
+++ src/lib.rs
@@ -1,101 +1,190 @@
+use std::{
+    borrow::Cow,
+};
+
 use failure::{self, Error};
+use coffeegrains::{Ident, Import, Parse, CowParse, Scoped};
 
-use coffeegrains::{Parse, Import};
+type cbstr<'a> = std::borrow::Cow<'a, [u8]>;
 
-pub fn prune_imports(&mut Parse) -> Result<(), Error> {
-    /*
-     * 1. List all imports
-     * 2. List symbols that should be imported
-     *    - Will need to check whether they were defined in the same package
-     *    - Which will require some kind of project structure
-     * 3. Take the symmetric difference
-     * 4. Remove non-star imports that remain
-     *    - This of course will not remove unneeded star imports
-     */
-    unimplemented!()
-}
+pub fn fix_imports<'a>(parse: &mut CowParse, project: &'a [Parse<'a>]) -> Result<(), Error> {
+    let project = project.iter().map(Parse::clone).map(Parse::into_cow).collect::<Vec<_>>();
 
-pub fn fill_in_imports(&mut Parse) -> Result<(), Error> {
-    /*
-     * 1. List all imports
-     * 2. List symbols that should be imported
-     *    - Will need to check whether they were defined in the same package
-     *    - Which will require some kind of project structure
-     * 3. Take the symmetric difference
-     * 4. For symbols that are not imported, search project for if they're imported elsewhere
-     * 5. Add these imports to the file
-     * 6. If any imports are made redundant by star imports, remove them
-     */
-    unimplemented!()
-}
+    const PRELUDE: &[cbstr] = &[
+        Cow::Borrowed(b"void"),
+        Cow::Borrowed(b"int"),
+        Cow::Borrowed(b"long"),
+        Cow::Borrowed(b"float"),
+        Cow::Borrowed(b"double"),
+        Cow::Borrowed(b"String"),
+    ];
 
-pub fn format_imports(parse: &Parse) -> Result<String, Error> {
-    use std::fmt::Write;
+    let all_imports = list_all_imports(&project);
+    let symbols_in_package = list_symbols_in_package(&project, &parse.package);
+    let external_symbols = find_external_symbols(parse);
 
-    fn format_import(import: &Import) -> String {
-        let static_label = if import.is_static { "static " } else { "" };
-        let star_label = if import.star { ".*" } else { "" };
-        let path = import.path.0.join(&b'.');
-        format!(
-            "import {}{}{};",
-            static_label,
-            unsafe { std::str::from_utf8_unchecked(&path) },
-            star_label
-        )
-    }
-
-    let statics = parse.imports.iter().filter(|import| !import.is_static);
-    let main_imports = statics
-        .clone()
-        .filter(|import| import.path.0[0] != b"java" && import.path.0[0] != b"javax")
-        .map(format_import)
-        .collect::<Vec<_>>();
-    let javax_imports = statics
-        .clone()
-        .filter(|import| import.path.0[0] == b"javax")
-        .map(format_import)
-        .collect::<Vec<_>>();
-    let java_imports = statics
-        .filter(|import| import.path.0[0] == b"java")
-        .map(format_import)
-        .collect::<Vec<_>>();
-    let statics = parse
-        .imports
+    let non_imported_symbols = external_symbols
         .iter()
-        .filter(|import| import.is_static)
-        .map(format_import)
+        .filter(|&&symbol| {
+            let base = &symbol.0[0];
+            let base_imported = parse
+                    .imports
+                    .iter()
+                    .any(|import| Some(&symbol.0[0]) == import.path.0.last())
+                    || symbols_in_package.contains(&&symbol.0[0])
+                    || PRELUDE.contains(&symbol.0[0].0);
+            let full_path_imported = parse.imports.iter().any(|import| &import.path == symbol);
+
+            !(base_imported || full_path_imported)
+        })
         .collect::<Vec<_>>();
 
-    let mut buffer = String::new();
-    let b = &mut buffer;
+    // TODO: Only return missing imports
+    Ok(())
+}
 
-    if !main_imports.is_empty() {
-        for import in main_imports {
-            writeln!(b, "{}", &import)?;
+fn list_all_imports<'a>(project: &'a [CowParse<'a>]) -> Vec<&'a Import<cbstr<'a>>> {
+    let mut imports = vec![];
+
+    for parse in project {
+        for import in &parse.imports {
+            imports.push(import);
         }
-        writeln!(b)?;
     }
 
-    if !javax_imports.is_empty() {
-        for import in javax_imports {
-            writeln!(b, "{}", &import)?;
+    imports
+}
+
+fn list_symbols_in_package<'a>(
+    project: &'a [CowParse<'a>],
+    package: &'a Scoped<cbstr<'a>>,
+) -> Vec<&'a Ident<cbstr<'a>>> {
+    let mut symbols = vec![];
+    for parse in project.iter().filter(|parse| &parse.package == package) {
+        for class in &parse.classes {
+            symbols.push(&class.name);
         }
-        writeln!(b)?;
+    }
+    symbols
+}
+
+fn find_external_symbols<'a>(parse: &'a CowParse<'a>) -> Vec<&'a Scoped<cbstr<'a>>> {
+    let mut types = vec![];
+
+    for class in &parse.classes {
+        for annotation in &class.annotations {
+            types.push(annotation);
+        }
+        for field in &class.fields {
+            for annotation in &field.annotations {
+                types.push(annotation);
+            }
+            types.push(&field.field_type);
+        }
+        for method in &class.methods {
+            for annotation in &method.annotations {
+                types.push(annotation);
+            }
+            types.push(&method.return_type);
+            for throws in &method.throws {
+                types.push(throws);
+            }
+
+            for arg in &method.args {
+                for annotation in &arg.annotations {
+                    types.push(annotation);
+                }
+                types.push(&arg.arg_type);
+            }
+        }
     }
 
-    if !java_imports.is_empty() {
-        for import in java_imports {
-            writeln!(b, "{}", &import)?;
+    let mut symbols = vec![];
+    while let Some(ty) = types.pop() {
+        symbols.push(&ty.type_name);
+        for param in &ty.type_params {
+            types.push(param);
         }
-        writeln!(b)?;
     }
 
-    if !statics.is_empty() {
-        for import in statics {
-            writeln!(b, "{}", &import)?;
-        }
-        writeln!(b)?;
-    }
+    symbols
+}
 
-    Ok(buffer)
+pub fn format_imports<'a>(parse: &CowParse<'a>, _additional: &[Import<&'a [u8]>]) -> Result<String, Error> {
+//     use std::fmt::Write;
+// 
+//     fn format_import(import: &Import<&[u8]>) -> String {
+//         let static_label = if import.is_static { "static " } else { "" };
+//         let star_label = if import.star { ".*" } else { "" };
+//         format!(
+//             "import {}{}{};",
+//             static_label,
+//             import.path,
+//             star_label
+//         )
+//     }
+// 
+//     let imports = parse.imports.iter();
+//     let non_statics = imports.clone().filter(|import| !import.is_static);
+//     let mut main_imports = non_statics
+//         .clone()
+//         .filter(|import| import.path.0[0] != b"java" && import.path.0[0] != b"javax")
+//         .map(format_import)
+//         .collect::<Vec<_>>();
+//     let mut javax_imports = non_statics
+//         .clone()
+//         .filter(|import| import.path.0[0] == b"javax")
+//         .map(format_import)
+//         .collect::<Vec<_>>();
+//     let mut java_imports = non_statics
+//         .filter(|import| import.path.0[0] == b"java")
+//         .map(format_import)
+//         .collect::<Vec<_>>();
+//     let mut statics = parse.imports.iter()
+//         .filter(|import| import.is_static)
+//         .map(format_import)
+//         .collect::<Vec<_>>();
+// 
+//     main_imports.sort();
+//     main_imports.dedup();
+//     javax_imports.sort();
+//     javax_imports.dedup();
+//     java_imports.sort();
+//     java_imports.dedup();
+//     statics.sort();
+//     statics.dedup();
+// 
+//     let mut buffer = String::new();
+//     let b = &mut buffer;
+// 
+//     if !main_imports.is_empty() {
+//         for import in main_imports {
+//             writeln!(b, "{}", &import)?;
+//         }
+//         writeln!(b)?;
+//     }
+// 
+//     if !javax_imports.is_empty() {
+//         for import in javax_imports {
+//             writeln!(b, "{}", &import)?;
+//         }
+//         writeln!(b)?;
+//     }
+// 
+//     if !java_imports.is_empty() {
+//         for import in java_imports {
+//             writeln!(b, "{}", &import)?;
+//         }
+//         writeln!(b)?;
+//     }
+// 
+//     if !statics.is_empty() {
+//         for import in statics {
+//             writeln!(b, "{}", &import)?;
+//         }
+//         writeln!(b)?;
+//     }
+// 
+//     Ok(buffer)
+    unimplemented!()
 }
