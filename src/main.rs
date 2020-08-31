0f64613e3e602fe8548c27bb78ad46c0eca035ca
Vi <1149443+mistodon@users.noreply.github.com>
2020-08-31 12:48:02 +0100

    WIP checkin

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
+// TODO: All failures should print the original file
+
 fn main() {
     use std::io::Read;
+
+    let project = coffeegrains::read_project(&std::env::current_dir().unwrap()).unwrap();
+    let project = coffeegrains::parse_project(&project).unwrap();
 
     let mut input = String::new();
     std::io::stdin().read_to_string(&mut input).unwrap();
 
-    let parse = coffeegrains::parse_file(&input).unwrap();
-    let formatted_imports = instantcoffee::format_imports(&parse).unwrap();
+    let mut parse = coffeegrains::parse_file(&input, None).unwrap().into_cow();
+    println!("{}", parse);
     let [start, end] = parse.import_span;
+
+    let new_imports = instantcoffee::fix_imports(&mut parse, &project).unwrap();
+
+    let formatted_imports = instantcoffee::format_imports(&parse, &[]).unwrap();
     println!("{}{}{}", &input[..start], formatted_imports, &input[end..]);
 }
